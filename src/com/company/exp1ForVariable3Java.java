39ce57749aa57c22b69f75df4d4fa505ea1def3d
soha-sadeghi <97523339+soha-sadeghi@users.noreply.github.com>
2022-03-16 17:21:07 +0330

    feat : added int a and float b.

--- src/com/company/exp1ForVariable3Java.java
+++ src/com/company/exp1ForVariable3Java.java
@@ -1,7 +1,9 @@
 package com.company;
 
 public class exp1ForVariable3Java {
-
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        int a=1000;
+        float b=5.004f;
+         System.out.println("a="+a+"\n"+"b="+b);
     }
 }
